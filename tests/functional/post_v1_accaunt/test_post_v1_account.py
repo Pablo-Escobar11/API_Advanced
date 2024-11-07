eecdebc505860790dbb37c7bdedd94bbaf644f4d
Pablo-Escobar11 <ermalovich7578@gmail.com>
2024-11-07 19:22:49 +0300

    two test

--- tests/functional/post_v1_accaunt/test_post_v1_account.py
+++ tests/functional/post_v1_accaunt/test_post_v1_account.py
@@ -1,19 +1,21 @@
-import pprint
-
-import requests
 from json import loads
 from datetime import datetime
+
+from dm_api_account.apis.accaunt_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mail_hog_api import MailhogApi
 
 
 def test_v1_account():
     # Регистрация пользователя!!!!!!!!!!!!!!!!!!!!!!!!!
+    accaunt_api = AccountApi(host='http://5.63.153.31:5051')
+    login_api = LoginApi(host='http://5.63.153.31:5051')
+    mail_hog_api = MailhogApi(host='http://5.63.153.31:5025')
     current_time = datetime.now()
     formatted_time = current_time.strftime("%m_%d_%H_%M_%S")
     login = f'pasha_{formatted_time}'
     email = f'{login}@mail.ru'
     password = 'Qwerty1'
-    print(login)
-    print(email)
 
     json_data = {
         'login': login,
@@ -21,33 +23,20 @@ def test_v1_account():
         'password': password,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
+    response = accaunt_api.post_v1_account(json_data=json_data)
     assert response.status_code == 201, f"Пользователь не был создан, {response.json()}"
 
     # Получить письма !!!!!!!!!!!!!!!!!!!!!!!!
 
-    params = {
-        'limit': '50',
-    }
-
-    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
+    response = mail_hog_api.get_api_v2_messages()
     assert response.status_code == 200, f"Письма не были получены, {response.json()}"
 
     # Получить токен !!!!!!!!!!!
-    token = None
-    for item in response.json()['items']:
-        user_data = loads(item['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            assert token is not None, f"Токен для пользователя {login} не был получен"
+    token = get_activation_token_by_login(login, response)
+    assert token is not None, f"Токен для пользователя {login} не был получен"
 
     # Активация пользователя
-    headers = {
-        'accept': 'text/plain',
-    }
-
-    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}', headers=headers)
+    response = accaunt_api.put_v1_account_token(token=token)
     assert response.status_code == 200, f'Пользователь не активен {response.json()}'
 
     # Авторизация !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -58,5 +47,14 @@ def test_v1_account():
         'rememberMe': True,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     assert response.status_code == 200, f'Пользователь не авторизован {response.json()}'
+
+def get_activation_token_by_login(login, response):
+    token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+        return token
